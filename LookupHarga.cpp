757f7d8db68f516a724d206cb85b5cc56f155746
Sir0ncy <rafiraff2005@gmail.com>
2025-11-27 20:39:19 +0700

    Update from static array to vector, edit all function also to parse vector

--- LookupHarga.cpp
+++ LookupHarga.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <climits>
 #include <string>
+#include <vector>
+#include <iomanip>
 
 struct Barang {
     std::string nama;
@@ -20,36 +22,70 @@ std::string toLowerCase(std::string s) {
 }
 
 // Method tambah barang
-void addBarang(Barang barang[], int &jumlahBarang, std::string nama, std::string kategori, int harga) {
-    barang[jumlahBarang].nama = nama;
-    barang[jumlahBarang].kategori = kategori;
-    barang[jumlahBarang].harga = harga;
-    jumlahBarang++;
+void addBarang(std::vector<Barang>& dataBarang) {
+    Barang b;
+    int hargaInput;
+    std::string namaInput, kategoriInput;
+
+    std::cout << "Nama barang: ";
+    std::getline(std::cin, namaInput);
+    std::cout << "Kategori   : ";
+    std::getline(std::cin, kategoriInput);
+    std::cout << "Harga      : ";
+    std::cin >> hargaInput;
+    std::cin.ignore();
+
+    b.nama = namaInput;
+    b.kategori = kategoriInput;
+    b.harga = hargaInput;
+
+    dataBarang.push_back(b);
 }
 
 // Method show semua daftar barang
-void showAllBarang(Barang barang[], int n) {
-    for (int i = 0; i < n; i++) {
-        std::cout << "===========================================\n";
-        std::cout << "Nama barang: " << barang[i].nama << std::endl;
-        std::cout << "Kategori: " << barang[i].kategori << std::endl;
-        std::cout << "Harga: Rp. " << barang[i].harga << std::endl;
+void showAllBarang(const std::vector<Barang> dataBarang) {
+    std::cout << "=============================================\n";
+    std::cout << std::left
+              << std::setw(5)  << "No"
+              << std::setw(20) << "Nama Barang"
+              << std::setw(15) << "Kategori"
+              << std::setw(10) << "Harga"
+              << "\n";
+    std::cout << "=============================================\n";
+
+    for (std::size_t i = 0; i < dataBarang.size(); i++) {
+        std::cout << std::left
+                  << std::setw(5)  << i + 1
+                  << std::setw(20) << dataBarang[i].nama
+                  << std::setw(15) << dataBarang[i].kategori
+                  << std::setw(10) << dataBarang[i].harga
+                  << "\n";
     }
 }
 
 // Method cari barang berdasarkan nama
-void findBarangNama(Barang barang[], int n, std::string findNama) {
+void findBarangNama(const std::vector<Barang>& dataBarang, std::string& findNama) {
     std::string key = toLowerCase(findNama);
     bool find = false;
 
-    for (int i = 0; i < n; i++) {
-        std::string namaBarangLower = toLowerCase(barang[i].nama);
-
-        if (namaBarangLower.find(key) != std::string::npos) {
-            std::cout << "===========================================\n";
-            std::cout << "Nama barang: " << barang[i].nama << std::endl;
-            std::cout << "Kategori: " << barang[i].kategori << std::endl;
-            std::cout << "Harga: Rp. " << barang[i].harga << std::endl;
+    std::cout << "=============================================\n";
+    std::cout << std::left
+              << std::setw(5)  << "No"
+              << std::setw(20) << "Nama Barang"
+              << std::setw(15) << "Kategori"
+              << std::setw(10) << "Harga"
+              << "\n";
+    std::cout << "=============================================\n";
+    int nomor = 1;
+    for (std::size_t i = 0; i < dataBarang.size(); i++) {
+        std::string lowerNama = toLowerCase(dataBarang[i].nama);
+        if (lowerNama.find(key) != std::string::npos) {
+            std::cout << std::left
+                      << std::setw(5)  << nomor++
+                      << std::setw(20) << dataBarang[i].nama
+                      << std::setw(15) << dataBarang[i].kategori
+                      << std::setw(10) << dataBarang[i].harga
+                      << "\n";
             find = true;
         }
     }
@@ -60,41 +96,29 @@ void findBarangNama(Barang barang[], int n, std::string findNama) {
 }
 
 // Method simpan semua harga barang sesuai kategori yang dicari
-int getHargaByKategori(Barang barang[], int &jumlahBarang, std::string kategori, int hargaKategori[], int indexKategori[]) {
-    int count = 0;
-
-    std::string key = toLowerCase(kategori);
-    for (int i = 0; i < jumlahBarang; i++) {
-        std::string kategoriLower = toLowerCase(barang[i].kategori);
-
-        if (kategoriLower.compare(key) == 0) {
-            hargaKategori[count] = barang[i].harga;
-            indexKategori[count] = i; // simpan index struct
-            count++;
-        }
+int getHargaByKategori(const std::vector<Barang>& dataBarang, std::string kategori, std::vector<int> hargaKategori) {
+    hargaKategori.clear();
+    for (int i = 0; i < dataBarang.size(); i++) {
+        if (dataBarang[i].kategori == kategori) hargaKategori.push_back(dataBarang[i].harga);
     }
-    return count;
 }
 
 // Method Insertion Sorting Ascending Parallel untuk sorting index yang dihasilkan method getHargaByKategori
-void insertionSortParallel(int arr[], int indexArr[], int n) {
-    for (int i = 1; i < n; i++) {
+void insertionSortParallel(std::vector<int>& arr) {
+    for (int i = 1; i < arr.size(); i++) {
         int temp = arr[i];
-        int tempIdx = indexArr[i];
         int j = i - 1;
         while (j >= 0 && arr[j] > temp) {
             arr[j + 1] = arr[j];
-            indexArr[j + 1] = indexArr[j];
             j--;
         }
         arr[j + 1] = temp;
-        indexArr[j + 1] = tempIdx;
     }
 }
 
 // Method Searching
-int binarySearch(int arr[], int n, int target) {
-    int left = 0, right = n - 1;
+int binarySearch(const std::vector<int>& arr, int target) {
+    int left = 0, right = arr.size() - 1;
     int closestIndex = -1; // index terdekat jika tidak ada match exact harga, -1 jika data tidak ditemukan
     int minDiff = INT_MAX; // untuk mencari selisih terkecil
     while (left <= right) {
@@ -116,126 +140,7 @@ int binarySearch(int arr[], int n, int target) {
 }
 
 int main() {
-    int n;
-    int jumlahBarang = 0;
-
-    std::cout << "Masukkan maksimum barang: ";
-    while (!(std::cin >> n) || n <= 0) {
-        std::cin.clear();
-        std::cin.ignore(1000, '\n');
-        std::cout << "Masukkan angka yang valid: ";
-    }
-
-    Barang dataBarang[n];
-    int hargaKategori[n];
-    int indexKategori[n];
-
-    int pilih = 0;
-
-    while (true) {
-        std::cout << "\nMenu:\n";
-        std::cout << "1. Tambah data Barang\n";
-        std::cout << "2. Tampilkan semua data Barang\n";
-        std::cout << "3. Cari barang berdasarkan nama\n";
-        std::cout << "4. Cari harga berdasarkan kategori\n";
-        std::cout << "5. Keluar\n";
-        std::cout << "Pilih: ";
-
-        while (!(std::cin >> pilih)) {
-            std::cin.clear();
-            std::cin.ignore(1000, '\n');
-            std::cout << "Masukkan angka yang valid: ";
-        }
-
-        std::cin.ignore();
-
-        if (pilih == 1) {
-            if (jumlahBarang >= n) {
-                std::cout << "Kapasitas penuh\n";
-                continue;
-            }
-            std::string nama, kategori;
-            int harga;
-
-            std::cout << "Nama barang: ";
-            std::getline(std::cin, nama);
-
-            std::cout << "Kategori: ";
-            std::getline(std::cin, kategori);
-
-            std::cout << "Harga: ";
-            while (!(std::cin >> harga) || harga < 0) {
-                std::cin.clear();
-                std::cin.ignore(1000, '\n');
-                std::cout << "Masukkan harga valid: ";
-            }
-            std::cin.ignore();
-
-            addBarang(dataBarang, jumlahBarang, nama, kategori, harga);
-        }
-
-        else if (pilih == 2) {
-            if (jumlahBarang == 0) {
-                std::cout << "Belum ada data\n";
-                continue;
-            }
-            showAllBarang(dataBarang, jumlahBarang);
-        }
-
-        else if (pilih == 3) {
-            if (jumlahBarang == 0) {
-                std::cout << "Belum ada data\n";
-                continue;
-            }
-            std::string nama;
-            std::cout << "Masukkan nama barang: ";
-            std::getline(std::cin, nama);
-            findBarangNama(dataBarang, jumlahBarang, nama);
-        }
-
-        else if (pilih == 4) {
-            if (jumlahBarang == 0) {
-                std::cout << "Belum ada data\n";
-                continue;
-            }
-            std::string kategori;
-            std::cout << "Masukkan kategori: ";
-            std::getline(std::cin, kategori);
-
-            int jumlah = getHargaByKategori(dataBarang, jumlahBarang, kategori, hargaKategori, indexKategori);
-
-            if (jumlah == 0) {
-                std::cout << "Tidak ada data kategori tersebut\n";
-                continue;
-            }
-
-            insertionSortParallel(hargaKategori, indexKategori, jumlah);
-
-            int target;
-            std::cout << "Masukkan harga yang dicari: ";
-            while (!(std::cin >> target) || target < 0) {
-                std::cin.clear();
-                std::cin.ignore(1000, '\n');
-                std::cout << "Harga tidak valid: ";
-            }
-            std::cin.ignore();
-
-            int idx = binarySearch(hargaKategori, jumlah, target); // BinSeach mereturn index harga match/terdekat dari array hargaKategori
-            int realIndexStruct = indexKategori[idx]; // Dari index diatas kita bisa tentukan posisi index struct aslinya yang disimpan di array indexKategori
-            std::cout << "============PENCARIAN HARGA============\n";
-            std::cout << "Nama Barang: " << dataBarang[realIndexStruct].nama << "\n";
-            std::cout << "Kategori: " << dataBarang[realIndexStruct].kategori << "\n";
-            std::cout << "Harga: Rp. " << dataBarang[realIndexStruct].harga << "\n";
-        }
-
-        else if (pilih == 5) {
-            break;
-        }
-
-        else {
-            std::cout << "Menu tidak valid\n";
-        }
-    }
-
+    
+    
     return 0;
 }
